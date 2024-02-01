e4da6640ea795207a4aa759422df86f9fc7b2639
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2024-02-01 19:18:37 -0300

    Se cambió el título de index.html y además se agregó un favicon

--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -24,21 +24,21 @@ const SectionHome = () => {
         </div>
     </section> */}
             < section >
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-                        <div class="sm:w-1/2 mb-10 px-4">
-                            <h2 class="title-font text-3xl font-bold text-[#008DDE] mt-6 mb-3 text-center">DIÁGNOSTICO</h2>
-                            <div class="text-center w-80 mx-auto">
-                                <p class="leading-relaxed text-black text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -mx-4 -mb-10 text-center">
+                        <div className="sm:w-1/2 mb-10 px-4">
+                            <h2 className="title-font text-3xl font-bold text-[#008DDE] mt-6 mb-3 text-center">DIÁGNOSTICO</h2>
+                            <div className="text-center w-80 mx-auto">
+                                <p className="leading-relaxed text-black text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                             </div>
-                            <button class="flex mx-auto mt-6 text-white text-2xl font-bold bg-[#6EAD15] border-0 py-2 px-7 rounded-full">COMENZAR</button>
+                            <button className="flex mx-auto mt-6 text-white text-2xl font-bold bg-[#6EAD15] border-0 py-2 px-7 rounded-full">COMENZAR</button>
                         </div>
-                        <div class="sm:w-1/2 mb-10 px-4">
-                            <h2 class="title-font text-3xl font-bold text-[#008DDE] mt-6 mb-3">BLOG</h2>
-                            <div class="text-center w-80 mx-auto">
-                                <p class="leading-relaxed text-black text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                        <div className="sm:w-1/2 mb-10 px-4">
+                            <h2 className="title-font text-3xl font-bold text-[#008DDE] mt-6 mb-3">BLOG</h2>
+                            <div className="text-center w-80 mx-auto">
+                                <p className="leading-relaxed text-black text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                             </div>
-                            <button class="flex mx-auto mt-6 text-white text-2xl font-bold bg-[#E97D00] border-0 py-2 px-20 rounded-full">IR</button>
+                            <button className="flex mx-auto mt-6 text-white text-2xl font-bold bg-[#E97D00] border-0 py-2 px-20 rounded-full">IR</button>
                         </div>
                     </div>
                 </div>
