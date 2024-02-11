1f5ed7ddc1ae9e570a0abfd20c9c9d9588ed0b98
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2024-02-11 20:59:32 -0300

    Se creó la página de registro usuario con su vista responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
 const Footer = () => {
     return (
-        <footer class="bg-[#EBEBEB]">
-            <div class="container flex items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
-                <div class="flex flex-row mx-7">
-                    <a href="#" class="mx-2" aria-label="mail" target="_blank" rel="noreferrer nopeener">
+        <footer className="bg-[#EBEBEB]">
+            <div className="container flex items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
+                <div className="flex flex-row mx-7">
+                    <a href="#" className="mx-2" aria-label="mail" target="_blank" rel="noreferrer nopeener">
                         <img src="/assets/img/skill-icons_email.png" alt="" />
                     </a>
-                    <p class="text-3xl ml-5 mr-9 pt-1 text-[#893685] font-bold">Contacto</p>
+                    <p className="text-3xl ml-5 mr-9 pt-1 text-[#893685] font-bold">Contacto</p>
                 </div>
                 <a href="#">
-                    <img class="w-auto h-9 ml-9 pl-9 items-center" src="assets/img/logo-footer.png" alt="" />
+                    <img className="w-auto h-9 ml-9 pl-9 items-center" src="assets/img/logo-footer.png" alt="" />
                 </a>
-                <div class="flex mx-5 px-3">
-                    <a href="https://www.facebook.com/lofworkcl" class="mx-6 px-3" aria-label="facebook" target="_blank" rel="noreferrer nopeener">
+                <div className="flex mx-5 px-3">
+                    <a href="https://www.facebook.com/lofworkcl" className="mx-6 px-3" aria-label="facebook" target="_blank" rel="noreferrer nopeener">
                         <img src="/assets/img/skill-icons_facebook.png" alt="facebook" />
                     </a>
-                    <a href="https://www.instagram.com/lofwork/" class="mx-5 px-3" aria-label="instagram" target="_blank" rel="noreferrer nopeener">
+                    <a href="https://www.instagram.com/lofwork/" className="mx-5 px-3" aria-label="instagram" target="_blank" rel="noreferrer nopeener">
                         <img src="/assets/img/skill-icons_instagram.png" alt="instagram" />
                     </a>
-                    <a href="https://www.linkedin.com/company/lofwork/" class="mx-5 px-3" aria-label="linkedin" target="_blank" rel="noreferrer nopeener">
+                    <a href="https://www.linkedin.com/company/lofwork/" className="mx-5 px-3" aria-label="linkedin" target="_blank" rel="noreferrer nopeener">
                         <img src="/assets/img/skill-icons_linkedin.png" alt="linkedin" />
                     </a>
                 </div>
