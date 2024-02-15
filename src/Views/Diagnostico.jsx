1559ad827201c971ec350b4b4aa75d1394e3f6ea
edictomanuelcortezramirez <devemcr@gmail.com>
2024-02-15 09:47:08 -0300

    feat:Crear vista mobile de pagina Diagnostico

--- src/Views/Diagnostico.jsx
+++ src/Views/Diagnostico.jsx
@@ -15,32 +15,30 @@ const Diagnostico = () => {
   return (
     <>
       <div className=" wrapper grip grap-8 justify-items-center items-center text-center">
-        <h3 className="text-3xl font-bold my-7 sm:text-xl">Diagnóstico</h3>
-        <p className="text-base font-light leading-5  mb-5 max-w-7xl mx-auto text-center sm:text-sm sm:w-11/12">
+        <h3 className=" font-bold py-10 max-sm:text-3xl sm:text-3xl md:text-4xl">
+          Diagnóstico
+        </h3>
+        <p className="text-2xl font-light leading-5  pb-8 max-w-7xl mx-auto text-justify max-sm:text-xl max-sm:w-10/12 sm:text-xl sm:w-10/12 md:text-2xl">
           Nuestra encuesta está diseñada para apoyar a emprendedores como tú a
           obtener una
-          <span className="font-bold text-base sm:text-sm">
+          <span className="font-bold text-2xl max-sm:text-xl max-sm:font-light sm:font-light sm:text-xl md:text-2xl">
             {' '}
             visión clara de su situación actual
           </span>{' '}
           en diversos aspectos cruciales de sus negocios. Al responder
           honestamente a las{' '}
-          <span className="font-bold text-base sm:text-sm">
+          <span className="font-bold text-2xl max-sm:text-xl max-sm:font-light sm:font-light sm:text-xl md:text-2xl">
             18 preguntas
           </span>{' '}
           de nuestros seis principales pilares estratégicos, estarás trazando el
           camino hacia un crecimiento organizacional más sólido y sostenible.
         </p>
-        <div
-          className={
-            'max-w-7xl mx-auto text-center sm:w-11/12 flex md:visible flex-col  items-center sm:flex justify-between  '
-          }
-        >
+        <div className="max-w-5xl mx-auto text-center  flex flex-col items-center max-sm:w-10/12">
           {expanded && (
-            <p className="text-base font-light leading-5">
+            <p className="text-2xl font-light text-justify leading-5 sm:invisible max-sm:text-xl max-sm:font-light">
               {' '}
               La meta de nuestra encuesta es proporcionarte una{' '}
-              <span className="font-bold text-base sm:text-sm">
+              <span className="font-bold text-2xl max-sm:text-xl max-sm:font-light">
                 valiosa devolución basada en tus respuestas.
               </span>{' '}
               Queremos ofrecerte insights perspicaces que te permitan entender
@@ -50,17 +48,28 @@ const Diagnostico = () => {
             </p>
           )}
           <button
-            className="bg-tranparent text-black text-xs font-normal  rounded  ml-auto focus:outline-none sm:mt-2 md:invisible"
+            className="bg-tranparent text-black text-base font-normal rounded ml-auto focus:outline-none max-sm:mt-2 sm:invisible"
             onClick={handleToggle}
           >
             {expanded ? 'Leer menos ▲' : 'Leer más ▼'}
           </button>
+          <p className=" text-2xl font-light leading-7 absolute mb-5 max-w-7xl mx-auto text-justify sm:mb-4 sm:visible max-sm:invisible sm:font-light sm:text-xl sm:w-10/12 md:text-2xl">
+            {' '}
+            La meta de nuestra encuesta es proporcionarte una{' '}
+            <span className="font-bold text-2xl max-sm:text-sm sm:font-light sm:text-xl md:text-2xl">
+              valiosa devolución basada en tus respuestas.
+            </span>{' '}
+            Queremos ofrecerte insights perspicaces que te permitan entender
+            mejor tu posición actual en áreas claves de tu emprendimiento. Esta
+            información te brindará la guía necesaria para tomar decisiones
+            informadas y estratégicas.
+          </p>
         </div>
       </div>
-      <div className="my-6 mx-10">
+      <div className="my-6 sm:mt-20 mx-24 max-sm:mx-14 sm:mx-14 md:mx-20 lg:mx-24  xl:mx-28 2xl:mx-56">
         <Diagnostic />
       </div>
-      <p className="text-lg font-normal max-w-7xl mx-auto text-center sm:font-normal sm:text-sm sm:w-11/12">
+      <p className="text-lg font-normal  text-justify pb-20 pt-10 max-w-7xl mx-auto max-sm:font-normal max-sm:text-xl max-sm:w-10/12 sm:w-10/12 sm:text-xl md:text-2xl">
         Creemos en el poder de la información para impulsar el cambio positivo.
         Al unirte a nosotros, no solo contribuirás a tu propio crecimiento, sino
         que también formarás parte de una comunidad comprometida con el progreso
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,28 +9,28 @@ const Question = ({ question }) => {
   const renderInnerCircle = (isSelected) => {
     if (isSelected) {
       return (
-        <div className="absolute inset-2 bg-[#6EAD15] ring-4 rounded-full ring-[#6EAD15] sm:bg-[#E97D00] sm:ring-[#E97D00]"></div>
+        <div className="absolute inset-2  ring-4  max-sm-ring-4 max-sm:inset-2 rounded-full bg-[#6EAD15] ring-[#6EAD15] max-sm:bg-[#E97D00] max-sm:ring-[#E97D00] sm:bg-[#E97D00] sm:ring-[#E97D00] lg:bg-[#6EAD15] lg:ring-[#6EAD15]"></div>
       );
     }
     return null;
   };
   return (
     <div>
-      <h3 className="text-lg font-normal my-4 sm:text-sm">
+      <h3 className="text-xl font-normal my-4 max-sm:text-lg sm:text-lg md:text-2xl">
         {question.question.text}
       </h3>
-      <ul className="text-sm font-normal space-y-3 my-6">
+      <ul className="text-xl font-normal space-y-3 my-6 max-sm:text-base max-sm:text-normal sm:text-lg md:text-xl">
         {question.options.map((option) => (
           <li key={option.id}>
             <label
-              className={`flex items-center cursor-pointer font-semibold mb-10 w-[450px] ${
+              className={`flex items-center cursor-pointer font-semibold mb-10 w-[600px] max-sm:w-[450px] sm:w-[550px] md:w-[700px] ${
                 selectedOption === option.id
-                  ? 'bg-[#6EAD15] rounded-full ring-2  ring-[#6EAD15] w-40 text-slate-50 sm:bg-[#E97D00] sm:ring-[#E97D00]'
+                  ? 'bg-[#6EAD15] rounded-full ring-2  ring-[#6EAD15] w-30 text-slate-50 max-sm:bg-[#E97D00] max-sm:ring-[#E97D00] sm:bg-[#E97D00] sm:ring-[#E97D00]  lg:bg-[#6EAD15] lg:ring-[#6EAD15]'
                   : 'hover:bg-[#8DA16F] text-gray-500 hover:text-gray-600 rounded-full w-40 '
               }`}
             >
               <input
-                className="hidden "
+                className="hidden"
                 type="radio"
                 name={`question-${question.question.id}`}
                 value={option.id}
@@ -38,10 +38,10 @@ const Question = ({ question }) => {
                 onChange={() => handleOptionChange(option.id)}
               />
               <div
-                className={`w-6 h-6 mr-1 rounded-full transition duration-250 ease cursor-pointer relative ${
+                className={`w-7 h-7 mr-1 rounded-full transition duration-250 ease cursor-pointer relative max-sm:w-6 max-sm:h-6 ${
                   selectedOption === option.id
-                    ? 'bg-slate-50 border-2 border-[#6EAD15] sm:border-[#E97D00] '
-                    : 'bg-white border-2 border-[#8DA16F] text-gray-700 sm:border-[#E97D00] sm:border-opacity-50'
+                    ? 'bg-slate-50 border-2 border-[#6EAD15] max-sm:border-[#E97D00] sm:border-[#E97D00] lg:border-[#6EAD15] xl:border-[#6EAD15]'
+                    : 'bg-white border-2  border-[#8DA16F]  text-gray-700 max-sm:border-[#E97D00] max-sm:border-opacity-50 sm:border-[#E97D00] sm:border-opacity-50 lg:border-[#8DA16F] '
                 }`}
               >
                 {renderInnerCircle(selectedOption === option.id)}
