e029cc11d25d1847f7c6dc2c36b89bc38baa9b39
Fabian Daza <fabian.daza2020@gmail.com>
2024-02-20 18:23:19 -0300

    59-fix: Se agrega footer con correcciones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,50 @@
-import Email from "../assets/img/icons_email.png";
-import Instagram from "../assets/img/icons_instagram.png";
-import Facebook from "../assets/img/icons_facebook.png";
-import Linkedin from "../assets/img/icons_linkedin.png";
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {faSquareFacebook, faSquareInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Logo from "../assets/img/Logo.png"
 
-import Logo from "../assets/img/Logo.png";
+
+
 const Footer = () => {
-  return (
-    <footer className="bg-[var(--secondary)] w-[100%] flex items-center justify-between h-[50px] lg:h-[100px] absolute bottom-0">
-      <div className="flex items-center w-[30%] lg:w-[33%] px-5">
-        <a href="#">
-          <img src={Email} className="w-[30px]" alt="" />
-        </a>
-        <p className="p-3 font-black text-xl text-[var(--primary)] hidden lg:block">
-          Contacto
-        </p>
-      </div>
-      <div className="w-[30%] lg:w-[33%] flex justify-center">
-        <a href="#">
-          <img className="w-[100px] lg:w-[200px]" src={Logo} alt="" />
-        </a>
-      </div>
-      <div className="w-[40%] lg:w-[33%] flex items-center justify-end gap-2 px-3 lg:gap-5">
-        <a href="#">
-          <img src={Instagram} className="w-[30px]" alt="" />
-        </a>
-        <a href="#">
-          <img src={Facebook} className="w-[30px]" alt="" />
-        </a>
-        <a href="#">
-          <img src={Linkedin} className="w-[30px]" alt="" />
-        </a>
-      </div>
-    </footer>
-  );
-};
+    return (
+        <footer className="absolute bottom-0 w-[100%] max-h-[70px]">
+            <div className="flex justify-between py-2 lg:py-2 bg-[var(--secondary)] ">
+                <div className="flex flex-row pl-3 lg:pl-16 my-2 lg:my-4 h-[35%] lg:[h-40%] w-[20%] lg:w-[33%] ">
+                <a href="mailto:contacto@lofwork.cl" target="_blank" rel="noopener noreferrer" alt="ícono del correo">
+                <FontAwesomeIcon icon={faEnvelope}  className=" text-[var(--primary)] h-[30px] lg:h-10 fa-2x" /> 
+                </a>
+                <p className=" text-lg ml-2 mt-2 font-extrabold text-[var(--primary)] hidden lg:block">Contacto</p>                
+                </div>                  
+
+                <div className='flex justify-center my-2 lg:my-7 h-[35%] lg:[h-45%] lg:w-[33%]'>
+                    <img src={Logo} className="h-[30px] lg:h-7 " alt="logo de la empresa" />
+                </div>
+
+                <div className="flex justify-center gap-5 pr-3 lg:gap-16 my-2 lg:my-4 lg:w-[33%]">
+                    <div className="h-[35%] lg:[h-40%]">
+                        <a href="hhttps://www.facebook.com/lofworkcl" target="_blank" rel="noopener noreferrer" alt="ícono de facebook">
+                        <FontAwesomeIcon icon={faSquareFacebook} className="text-[var(--primary)] h-[30px] lg:h-10 fa-2x "/>
+                        </a>
+                    </div>
+
+                    <div className="h-[35%] lg:[h-40%]">
+                        <a href="https://www.instagram.com/lofwork/" target="_blank" rel="noopener noreferrer" alt="ícono de instagram">
+                        <FontAwesomeIcon icon={faSquareInstagram} className="text-[var(--primary)] h-[30px] lg:h-10 fa-2x "/>
+                        </a>
+                    </div>
+
+                    <div className="h-[35%] lg:[h-40%]">
+                        <a href="https://www.linkedin.com/company/lofwork/" target="_blank" rel="noopener noreferrer" alt="ícono de linkedin">
+                        <FontAwesomeIcon icon={faLinkedin} className=" text-[var(--primary)] h-[30px] lg:h-10 fa-2x "/>
+                        </a>
+                    </div>
+                </div>          
+            </div>
+
+
+        </footer>
+    )
+  }
 
 export default Footer;
